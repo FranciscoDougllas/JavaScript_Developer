34a2d9acadd101dc0d198745cf62b90073030f8e
Francisco Douglas <franciscodouglas77@outlook.com>
2024-05-16 14:31:50 -0300

    ------

---
+++ 2-Funcoes/1-funcao.js
@@ -0,0 +1,13 @@
+function quadrado(valor){
+    return valor * valor;
+}
+const num = Math.floor(Math.random() * 100) + 1;
+console.log(num)
+console.log(quadrado(num))
+
+
+//Organização de codigos
+function main(){
+    console.log('Programa Principal');
+}
+main()
---
+++ 2-Funcoes/2-imc.js
@@ -0,0 +1,30 @@
+//Funcao calcula imc
+function calculaImc(peso, altura){
+    return peso / (altura * altura);
+}
+
+
+const peso = 85;
+const altura = 1.67;
+let imc = calculaImc(peso, altura)
+
+console.log(`Seu IMC e de ${imc.toFixed(2)}`)
+
+//Condicional para o Programa
+function classifica_imc(imc){
+    }
+    if (imc < 17){
+        console.log('Abaixo do Peso');
+    }else if(imc > 17 && imc < 18.49){
+        console.log('Peso Normal');
+    }else if(imc > 18.5 && imc < 24.99){
+        console.log('Acima do Peso');
+    }else if(imc > 25 && imc < 34.99){
+        console.log('SObrePeso I')
+    }else if(imc > 35 && imc < 39.99){
+        console.log('SObrepeso II')
+    }else{
+        console.log('Obesidade Morbida')
+    }
+
+classifica_imc(imc);

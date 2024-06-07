fc30b382330d8f8bf28695c43a3af92eabf44371
Francisco Douglas <franciscodouglas77@outlook.com>
2024-06-07 14:46:29 -0300

    ------

---
+++ 1.1-Primeiros-Passos/entrada-Dados.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Qual é o seu nome? ', (resposta) => {
+    console.log(`Olá ${resposta}`);
+    rl.close();
+});
